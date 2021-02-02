de9764628050badc44cd31e2dae7ca214d300f12
supremequeen189 <luisa@luisazhou.com>
2021-02-01 20:09:23 -0500

    first commit

---
+++ test/spec.js
@@ -0,0 +1,20 @@
+const { expect } = require('chai');
+
+const app = require('supertest')(require('../app'));
+
+// describe('Routes', () => {
+//     describe('GET /', () => {
+//         it ('show information about the api', async() => {
+//             const response = await app.get('/');
+//             expect(response.status).to.equal(200); 
+//         })
+//     })
+// })
+
+describe('test test', () => {
+        describe('GET /', () => {
+            it ('you have a test!', async() => {
+        
+            })
+        })
+    })
