d1c845dc14fd96782035c44ed159fce7d287e5f8
supremequeen189 <luisa@luisazhou.com>
2021-02-01 20:23:56 -0500

    ready for travis

--- app.js
+++ app.js
@@ -2,3 +2,13 @@ const express = require('express');
 const app = express();
 
 module.exports = app;
+
+app.get('/', (req, res) => {
+    res.send(`
+        <html>
+            <body>
+            Hihihihihihihi!
+            </body>
+        </html>
+    `)
+})
