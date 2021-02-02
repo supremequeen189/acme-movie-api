ce10a01b5caf408bed626b8786f8793775dc9826
supremequeen189 <luisa@luisazhou.com>
2021-02-01 20:54:42 -0500

    updated routes and tests

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const app = express();
 
+const {models: {Actor, Movie, Role}} = require ('./db');
+
 module.exports = app;
 
 app.get('/', (req, res) => {
@@ -11,4 +13,20 @@ app.get('/', (req, res) => {
             </body>
         </html>
     `)
+})
+
+app.get('/api/movies', async(req, res, next) => {
+    try {
+        res.send(await Movie.findAll());
+    } catch(ex) {
+        next(ex);
+    }
+})
+
+app.get('/api/actors', async(req, res, next) => {
+    try {
+        res.send(await Actor.findAll());
+    } catch(ex) {
+        next(ex);
+    }
 })
--- test/spec.js
+++ test/spec.js
@@ -4,11 +4,25 @@ const { syncAndSeed } = require('../db');
 const app = require('supertest')(require('../app'));
 
 describe('Routes', () => {
-    before (() => syncAndSeed());
+    beforeEach(() => syncAndSeed());
 
     describe('GET /', () => {
         it ('show information about the api', async() => {
             const response = await app.get('/');
+            expect(response.status).to.equal(200); 
+        })
+    })
+
+    describe('GET /api/movies', () => {
+        it ('show all movies', async() => {
+            const response = await app.get('/api/movies');
+            expect(response.status).to.equal(200); 
+        })
+    })
+
+    describe('GET /api/actors', () => {
+        it ('show all actors', async() => {
+            const response = await app.get('/api/actors');
             expect(response.status).to.equal(200); 
         })
     })
