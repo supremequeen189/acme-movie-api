292a4156020975d7e160db8de18b75e2bc55baf8
supremequeen189 <luisa@luisazhou.com>
2021-02-01 20:29:59 -0500

    get / test added

--- test/spec.js
+++ test/spec.js
@@ -2,14 +2,14 @@ const { expect } = require('chai');
 
 const app = require('supertest')(require('../app'));
 
-// describe('Routes', () => {
-//     describe('GET /', () => {
-//         it ('show information about the api', async() => {
-//             const response = await app.get('/');
-//             expect(response.status).to.equal(200); 
-//         })
-//     })
-// })
+describe('Routes', () => {
+    describe('GET /', () => {
+        it ('show information about the api', async() => {
+            const response = await app.get('/');
+            expect(response.status).to.equal(200); 
+        })
+    })
+})
 
 describe('test test', () => {
         describe('GET /', () => {
@@ -17,4 +17,4 @@ describe('test test', () => {
         
             })
         })
-    })
+})
